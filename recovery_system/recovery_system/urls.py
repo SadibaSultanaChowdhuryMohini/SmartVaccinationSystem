6892e2239cbeec358bd77b6f1c8fdefb16f3541f
Ahnaf <23101191@uap-bd.edu>
2026-05-20 02:17:39 +0600

    Updated project with latest small adjustments

--- recovery_system/recovery_system/urls.py
+++ recovery_system/recovery_system/urls.py
@@ -1,16 +1,18 @@
+from accounts import views
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('accounts/', include('accounts.urls')),
-    path('recovery/', include('recovery.urls')),
-    path('vaccination/', include('vaccination.urls')),
-    path('clinics/', include('clinics.urls')),  # Links up their existing routes perfectly!
-]
 
+urlpatterns = [
+
+    path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+    path('accounts/', include('accounts.urls')),
+    path('vaccination/', include('vaccination.urls')),
+    path('recovery/', include('recovery.urls')),
+    path('clinics/', include('clinics.urls')),
+]
 if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
